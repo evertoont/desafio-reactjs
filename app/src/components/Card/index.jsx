8f80a43e09f5ceeb7cf49c578a7c9f5d20a9f1ff
Everton Carmo <evertoontcarmo@gmail.com>
2021-05-07 23:24:15 -0300

    move sidebar to the  profile and addning card repository

---
+++ app/src/components/Card/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { HiOutlineStar } from "react-icons/hi";
+import { BsDot } from "react-icons/bs";
+import styles from "./style.module.scss";
+
+export default function CardRepo() {
+  return (
+    <div className={styles.container}>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <h2>Repository Name</h2>
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
+        congue ligula in rutrum. Morbi nec lacus condimentum, hendrerit mi eu,
+        feugiat.
+      </p>
+      <div className={styles.info_repo}>
+        <span>
+          <HiOutlineStar /> 100 stars{" "}
+        </span>
+        <span>
+          <BsDot />
+        </span>
+        <span>Updadted 30days</span>
+      </div>
+      <hr/>
+    </div>
+  );
+}
--- app/src/pages/Profile/index.jsx
+++ app/src/pages/Profile/index.jsx
@@ -1,10 +1,100 @@
 import React from "react";
-import SideBar from "../../components/SideBar";
+import { useHistory } from "react-router";
+import styles from "./profile.module.scss";
+import CardRepo from "../../components/Card";
+import { AiOutlineMail, AiOutlineHeart, AiOutlineStar } from "react-icons/ai";
+import { FiTwitter, FiLink } from "react-icons/fi";
+import {
+  HiOutlineUserGroup,
+  HiOutlineLocationMarker,
+  HiOutlineOfficeBuilding,
+} from "react-icons/hi";
 
 export default function Profile() {
+  const history = useHistory();
+
   return (
-    <>
-      <SideBar />
-    </>
+    <div className={styles.container}>
+      <div className={styles.sideBar}>
+        <div className={styles.user_image}>
+          <img
+            src="https://avatars.githubusercontent.com/u/55769021?v=4"
+            alt="avatar user"
+          />
+        </div>
+        <div className={styles.content_description}>
+          <h1>Everton Oliver</h1>
+          <h2>
+            <a href="https://github.com/evertoont">@evertoont</a>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            tincidunt congue ligula in rutrum. Morbi nec lacus condimentum,
+            hendrerit mi eu, feugiat.
+          </p>
+          <div className={styles.user_status}>
+            <span>
+              <HiOutlineUserGroup /> 10 Followers
+            </span>
+            <span>
+              <AiOutlineHeart /> 10 Following
+            </span>
+            <span>
+              <AiOutlineStar /> 10 Stars
+            </span>
+          </div>
+          <div className={styles.user_info}>
+            <ul>
+              <li>
+                <HiOutlineOfficeBuilding /> Organization
+              </li>
+              <li>
+                <HiOutlineLocationMarker /> Location
+              </li>
+              <li>
+                <AiOutlineMail /> Email
+              </li>
+              <li>
+                <FiLink />
+                <a
+                  href="http://www.meusite.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  www.meusite.com
+                </a>
+              </li>
+              <li>
+                <FiTwitter />
+                <a
+                  href="https://twitter.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  @twitter
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.go_back}>
+            <button onClick={() => history.push("/")}>Voltar</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.repository}>
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+        <CardRepo />
+      </div>
+    </div>
   );
 }
